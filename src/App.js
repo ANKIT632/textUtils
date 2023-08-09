49f39fb39db88a62df2a56051dda2d9dcdf2122d
ankit gupta <ankitgupta17878@gmail.com>
2023-08-09 18:39:07 +0530

    fixed error

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
 
   return (
     <>
-      <Router basename='/textUtils'>
+      <Router basename='textUtils'>
         <Navbar title="Textutils" toggle={toggleMode} mode={mode}></Navbar>
         <Alert alert={alert} />
         <Routes>
